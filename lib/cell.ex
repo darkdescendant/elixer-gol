1bbac67267bfda87a6f3b361fa28a266759fe5ae
Joseph Jones <darkdescendant@fastmail.fm>
2016-03-24 09:22:21 -0700

    Changed cell to a GenServer

--- lib/cell.ex
+++ lib/cell.ex
@@ -1,31 +1,53 @@
 defmodule GOL.Cell do
-
+	use GenServer
+	
 	def start_link(cell_id, bounds) do
-		Agent.start_link(fn -> %{cell_id: cell_id, bounds: bounds, state: :dead} end)
+		GenServer.start_link(__MODULE__, %{cell_id: cell_id, bounds: bounds, state: :dead}, [])
+		#Agent.start_link(fn -> %{cell_id: cell_id, bounds: bounds, state: :dead} end)
 	end
 
 	def cell_id(cell) do
-		Agent.get(cell, &Map.get(&1, :cell_id))
+		{:ok, id} = GenServer.call(cell, {:cell_id})
+		id
 	end
 
 	def neighbors(cell) do
-		case Agent.get(cell, &Map.get(&1, :neighbors)) do
-			{:ok, n} -> n
-		  _ ->
-				{cx, cy} = GOL.Cell.cell_id(cell)
-				{bx, by} = Agent.get(cell, &Map.get(&1, :bounds))
-				n = for nx <- cx-1..cx+1, ny <- cy-1..cy+1, nx >= 0 && nx < bx, ny >= 0 && ny < by, !(nx == cx && ny == cy), do: {nx, ny}
-				Agent.update(cell, &Map.put(&1, :neighbors, n))
-				n
-		end
+		GenServer.call(cell, {:neighbors})
 	end
 
 	def set_state(cell, state) do
-		Agent.update(cell, &Map.put(&1, :state, state))
+		_ = GenServer.call(cell, {:set_state, state})
+		:ok
 	end
 
 	def get_state(cell) do
-		Agent.get(cell, &Map.get(&1, :state))
+		{:ok, state} = GenServer.call(cell, {:get_state})
+		state
+	end
+
+	def init(data) do
+		{:ok, data}
+	end
+
+	def handle_call(request, _from, data) do
+		case request do
+			{:cell_id} -> 
+				{:reply, Map.fetch(data, :cell_id), data}
+			{:get_state} ->
+				{:reply, Map.fetch(data, :state), data}
+			{:set_state, state} ->
+				{:reply, :ok, Map.put(data, :state, state)}
+			{:neighbors} ->
+				case Map.get(data, :neighbors) do
+					{:ok, n} -> n
+					_ ->
+						{:ok, {cx, cy}} = Map.fetch(data, :cell_id)
+						{:ok, {bx, by}} = Map.fetch(data, :bounds)
+						n = for nx <- cx-1..cx+1, ny <- cy-1..cy+1, nx >= 0 && nx < bx, ny >= 0 && ny < by, !(nx == cx && ny == cy), do: {nx, ny}
+						{:reply, n, Map.put(data, :neighbors, n)}
+				end
+		end
+		
 	end
 	
 end
